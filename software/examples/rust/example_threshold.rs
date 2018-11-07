560bf2c5b1211481fc54c4b0b8c502ee597af63f
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:32:23 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -12,10 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
                                           // Don't use device before ipcon is connected.
 
-    // Create receiver for power events.
-    let power_receiver = vc.get_power_receiver();
+    let power_receiver = vc.get_power_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `vc` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `vc` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for power in power_receiver {
