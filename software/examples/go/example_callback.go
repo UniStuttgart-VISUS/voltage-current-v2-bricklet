863d053b1ba558be856d6455840c201533cc8cc9
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:17:27 +0100

    Add go examples.

---
+++ software/examples/go/example_callback.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/voltage_current_v2_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Voltage/Current Bricklet 2.0.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	vc, _ := voltage_current_v2_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	vc.RegisterCurrentCallback(func(current int32) {
+		fmt.Printf("Current: %d A\n", float64(current)/1000.0)
+	})
+
+	// Set period for current callback to 1s (1000ms) without a threshold.
+	vc.SetCurrentCallbackConfiguration(1000, false, 'x', 0, 0)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
---
+++ software/examples/go/example_threshold.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/voltage_current_v2_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Voltage/Current Bricklet 2.0.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	vc, _ := voltage_current_v2_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	vc.RegisterPowerCallback(func(power int32) {
+		fmt.Printf("power: %d W\n", float64(power)/1000.0)
+	})
+
+	// Configure threshold for power "greater than 10 W"
+	// with a debounce period of 1s (1000ms).
+	vc.SetPowerCallbackConfiguration(1000, false, '>', 10*1000, 0)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
