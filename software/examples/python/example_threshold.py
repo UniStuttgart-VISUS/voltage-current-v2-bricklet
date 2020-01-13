6cf093677990209b46e10e2928e639ffd59cbf8f
Erik Fleckstein <erik@tinkerforge.com>
2020-01-13 17:25:03 +0100

    examples: Use python3 input instead of python2 raw_input.

--- software/examples/python/example_threshold.py
+++ software/examples/python/example_threshold.py
@@ -26,5 +26,5 @@ if __name__ == "__main__":
     # with a debounce period of 1s (1000ms)
     vc.set_power_callback_configuration(1000, False, ">", 10*1000, 0)
 
-    raw_input("Press key to exit\n") # Use input() in Python 3
+    input("Press key to exit\n") # Use raw_input() in Python 2
     ipcon.disconnect()
